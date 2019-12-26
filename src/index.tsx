6f430557dfdd12faf5e1ba3e11b549005440762e
himself65 <himself65@outlook.com>
2019-12-27 00:31:15 +0800

    feat: using parcel to build project

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-if (DEBUG) {
-  console.log('---DEVELOPMENT ENV---')
-} else {
-  console.log('---PRODUCTION ENV---')
-}
-
 const root = document.getElementById('root')!
 ReactDOM.createRoot(root).render(<App/>)
 
